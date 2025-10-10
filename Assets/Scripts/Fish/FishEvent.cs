97cb10128cf3befa428d952cb5e3cf5a5e17f8a9
NariakiShipo <156037460+NariakiShipo@users.noreply.github.com>
2025-10-11 02:37:51 +0800

    Update basic enviorment and create new game grab gold fish basic operator

---
+++ Assets/Scripts/Fish/FishEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FishEvent : MonoBehaviour
+{
+    [SerializeField]private GameEvent gameEvent;
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bucket"))
+        {
+           gameEvent.score += 1;
+            Debug.LogWarning("Fish caught! Score: " + gameEvent.score);
+              Destroy(gameObject);
+        }
+    }
+}
